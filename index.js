89a592aec58377e9b358f78e63d8573255f90c3f
Abel2407 <73433089+Abel2407@users.noreply.github.com>
2023-01-02 22:01:34 -0500

    nuevo index

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home(){
+    const listItems = [1,2,3,4]
+
+    const addItem = () => {
+        const variableA = 1;
+        const variableB = 2;
+        const variableC = 3;
+
+        return variableA*3 + variableC*variableB
+    }
+
+    return 
+    (
+        <><div>Home</div>
+        <p>Hola mundo</p><button onclick = {addItem} /></>
+    )
+}
+
+export default Home
